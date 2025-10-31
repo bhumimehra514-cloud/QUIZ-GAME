2fc591541b0b35dcc40179cfd0be8fb96ae72d7b
bhumimehra514-cloud <bhumimehra514@gmail.com>
2025-10-31 08:08:57 -0700

    Update QUIZ.py

--- QUIZ.py
+++ QUIZ.py
@@ -1,44 +1,4 @@
 import streamlit as st
-#st.write("Q1: First Alphabet of English language..?\nA) D\tB) I\nC) A\tD) f")
-#ans1 =st.text_input("Enter your answer here...")
-#st.write("your answer is ...",ans1)
-#st.write("<....................................>")
-#st.write("Q2: Which of these is a mammel..?\nA) Duck\tB) Hen\nC) Owl\tD) Whale")
-#ans2 =st.text_input("Enter your answer here...")
-#st.write("your answer is ...",ans2)
-#st.write("<....................................>")
-#st.write("Q3: Which is the largest ocean in the world?\nA) Indian ocean\tB) Pacific ocean\nC) Arctic ocean\tD) Atlantic ocean")
-#ans3 =st.text_input("Enter your answer here...")
-#st.write("your answer is ...",ans3)
-#st.write("<....................................>")
-#st.write("Q4: WHich is the National Animal ..?\nA) Fox\tB) Cow\nC) Tiger\tD) Elephant")
-#ans4 =st.text_input("Enter your answer here...")
-#st.write("your answer is ...",ans4)
-#st.write("<....................................>")
-#st.write("Q5: How many continents in the world..?\nA) 4\tB) 7\nC) 8\tD) 9")
-#ans5 =st.text_input("Enter your answer here...")
-#st.write("your answer is ...",ans5)
-#Total = 0 
-#if ans1=="c" or ans1 == "C":
- #    Total +=1
-#if ans2=="d" or ans2 == "D":
- #    Total +=1
-#if ans3=="b" or ans3 == "B":
- #    Total +=1
-#if ans4=="c" or ans4 == "C":
- #    Total +=1
-#if ans5=="b" or ans5 == "B":
- #    Total +=1
-#st.write(Total)
-#if Total ==5:
- #    st.write("Congratulations.... You are the topper with 5 points....")
-#elif Total == 4:
- #    st.write("Congratulations... You have scored Second position")
-#elif Total == 3:
- #    st.write("You have passed the Quiz wih 3 points.. Wish you very best for next time....  ")
-#else:
-#     st.write("Better Luck next time....")
-
 ques =["Q1: First Alphabet of English language..?\nA) d\tB) e\nC) a\tD) f","Q2: Which of these is a mammel..?\nA) Duck\tB) Hen\nC) Owl\tD) Whale","Q3: Which is the largest ocean in the world?\nA) Indian ocean\tB) Pacific ocean\nC) Arctic ocean\tD) Atlantic ocean","Q4: WHich is the National Animal ..?\nA) Fox\tB) Cow\nC) Tiger\tD) Elephant","Q5: How many continents in the world..?\nA) 4\tB) 7\nC) 8\tD) 9"]
 AL=[]
 for i in ques:
