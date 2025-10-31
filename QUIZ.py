339a2ed1a44b8d3cd625c99e93d1a20f0b79dd45
bhumimehra514-cloud <bhumimehra514@gmail.com>
2025-10-31 07:02:14 -0700

    Update QUIZ.py

--- QUIZ.py
+++ QUIZ.py
@@ -1,22 +1,23 @@
- print("Q1: First Alphabet of English language..?\nA) d\tB) e\nC) a\tD) f")
- ans1 = input("Enter your answer here...")
- print("your answer is ...",ans1)
- print("<....................................>")
- print("Q2: Which of these is a mammel..?\nA) Duck\tB) Hen\nC) Owl\tD) Whale")
- ans2 = input("Enter your answer here...")
- print("your answer is ...",ans2)
- print("<....................................>")
- print("Q3: Which is the largest ocean in the world?\nA) Indian ocean\tB) Pacific ocean\nC) Arctic ocean\tD) Atlantic ocean")
- ans3 = input("Enter your answer here...")
- print("your answer is ...",ans3)
- print("<....................................>")
- print("Q4: WHich is the National Animal ..?\nA) Fox\tB) Cow\nC) Tiger\tD) Elephant")
- ans4 = input("Enter your answer here...")
- print("your answer is ...",ans4)
- print("<....................................>")
- print("Q5: How many continents in the world..?\nA) 4\tB) 7\nC) 8\tD) 9")
- ans5 = input("Enter your answer here...")
- print("your answer is ...",ans5)
+import streamlit as st
+st.write("Q1: First Alphabet of English language..?\nA) d\tB) e\nC) a\tD) f")
+ans1 =st.text_input("Enter your answer here...")
+st.write("your answer is ...",ans1)
+st.write("<....................................>")
+st.write("Q2: Which of these is a mammel..?\nA) Duck\tB) Hen\nC) Owl\tD) Whale")
+ans2 =st.text_input("Enter your answer here...")
+st.write("your answer is ...",ans2)
+st.write("<....................................>")
+st.write("Q3: Which is the largest ocean in the world?\nA) Indian ocean\tB) Pacific ocean\nC) Arctic ocean\tD) Atlantic ocean")
+ans3 =st.text_input("Enter your answer here...")
+st.write("your answer is ...",ans3)
+st.write("<....................................>")
+st.write("Q4: WHich is the National Animal ..?\nA) Fox\tB) Cow\nC) Tiger\tD) Elephant")
+ans4 =st.text_input("Enter your answer here...")
+st.write("your answer is ...",ans4)
+st.write("<....................................>")
+st.write("Q5: How many continents in the world..?\nA) 4\tB) 7\nC) 8\tD) 9")
+ans5 =st.text_input("Enter your answer here...")
+st.write("your answer is ...",ans5)
  Total = 0 if ans1=="c" or ans1 == "C":
      Total +=1
  if ans2=="d" or ans2 == "D":
@@ -27,15 +28,15 @@
      Total +=1
  if ans5=="b" or ans5 == "B":
      Total +=1
- print(Total)
+ st.write(Total)
  if Total ==5:
-     print("Congratulations.... You are the topper with 5 points....")
+     st.write("Congratulations.... You are the topper with 5 points....")
  elif Total == 4:
-     print("Congratulations... You have scored Second position")
+     st.write"Congratulations... You have scored Second position")
  elif Total == 3:
-     print("You have passed the Quiz wih 3 points.. Wish you very best for next time....  ")
+     st.write("You have passed the Quiz wih 3 points.. Wish you very best for next time....  ")
  else:
-     print("Better Luck next time....")
+     st.write("Better Luck next time....")
     
 
    
