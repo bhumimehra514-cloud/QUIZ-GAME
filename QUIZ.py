a3ddc7bac42e76114621b18d6c75a48e94547a10
bhumimehra514-cloud <bhumimehra514@gmail.com>
2025-10-31 07:20:28 -0700

    Update QUIZ.py

--- QUIZ.py
+++ QUIZ.py
@@ -29,17 +29,16 @@ if ans4=="c" or ans4 == "C":
      Total +=1
 if ans5=="b" or ans5 == "B":
      Total +=1
- st.write(Total)
- if Total ==5:
+st.write(Total)
+if Total ==5:
      st.write("Congratulations.... You are the topper with 5 points....")
- elif Total == 4:
+elif Total == 4:
      st.write"Congratulations... You have scored Second position")
- elif Total == 3:
+elif Total == 3:
      st.write("You have passed the Quiz wih 3 points.. Wish you very best for next time....  ")
- else:
+else:
      st.write("Better Luck next time....")
     
 
    
 
-    print("Better Luck next ti
